5d288cf58c6dde7d1bd03ff63e09ac667646fbb5
LeoDog896 <LeoDog896@hotmail.com>
2021-06-11 15:39:27 -0400

    Add skin command

--- src/main/kotlin/world/cepi/npc/modification/SkinCommand.kt
+++ src/main/kotlin/world/cepi/npc/modification/SkinCommand.kt
@@ -2,26 +2,46 @@ package world.cepi.npc.modification
 
 import net.minestom.server.command.builder.Command
 import net.minestom.server.command.builder.arguments.ArgumentType
+import net.minestom.server.command.builder.exception.ArgumentSyntaxException
 import net.minestom.server.entity.Player
 import net.minestom.server.entity.PlayerSkin
 import world.cepi.kstom.command.addSyntax
 import world.cepi.kstom.command.arguments.literal
+import java.lang.Exception
+import java.util.*
 
 object SkinCommand : Command("skin") {
 
     init {
-        val set = "set".literal()
+        val uuidLiteral = "uuid".literal()
+        val selector = "selector".literal()
         val reset = "reset".literal()
 
         val user = ArgumentType.Entity("user")
             .onlyPlayers(true)
             .singleEntity(true)
 
-        addSyntax(set, user) { sender, args ->
+        val uuid = ArgumentType.Word("uuid").map {
+            try {
+                UUID.fromString(it)
+            } catch (e: Exception) {
+                throw ArgumentSyntaxException("Invalid uuid", it, 1)
+            }
+        }
+
+        addSyntax(selector, user) { sender, args ->
             val player = sender as Player
 
             player.skin = PlayerSkin.fromUuid(
                 args[user].find(player)[0].uuid.toString()
+            )
+        }
+
+        addSyntax(uuidLiteral, uuid) { sender, args ->
+            val player = sender as Player
+
+            player.skin = PlayerSkin.fromUuid(
+                args[uuid].toString()
             )
         }
 
